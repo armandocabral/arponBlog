661de2e9f79e95759685e49c339e5e5fcf05f054
kuai-mare carrasquero <orionkmc@gmail.com>
2020-09-30 11:44:51 -0400

    agregando gitignore y eliminando configuracion para que funcione en local

--- ablog/settings.py
+++ ablog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 import dj_database_url
 from decouple import config
 
@@ -141,9 +140,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-django_heroku.settings(locals())
